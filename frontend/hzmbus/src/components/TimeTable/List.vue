9629af8b94228cc7b9478820e76f17428a8d1721
xiaohuo <xiaohuo-desu@outlook.com>
2024-04-26 19:21:42 +0800

    update todo

--- frontend/hzmbus/src/components/TimeTable/List.vue
+++ frontend/hzmbus/src/components/TimeTable/List.vue
@@ -1,13 +1,14 @@
 <template>
-  <var-list
-    :finished="finished"
-    v-model:loading="loading"
-    @load="load"
-  >
-    <var-cell :key="item" v-for="item in list">
-      列表项: {{ item }}city: {{ city }}
-    </var-cell>
-  </var-list>
+    <var-list
+        :finished="finished"
+        v-model:loading="loading"
+        @load="load"
+    >
+        <var-cell :key="item" v-for="item in list">
+        <!--TODO: Filling real data-->
+        列表项: {{ item }}city: {{ city }}
+        </var-cell>
+    </var-list>
 </template>
 
 <script setup lang="ts">
@@ -17,6 +18,7 @@ defineProps(['city']);
 const loading = ref(false)
 const finished = ref(false)
 const list = ref<number[]>([])
+//TODO realize the time table computing function and move to a specific TS file
 function load() {
   setTimeout(() => {
     for (let i = 0; i < 20; i++) {
