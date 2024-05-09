cf7ae4a2b2a6d99fb82b48a8c70174237beae47a
Xiaohuo-Thinkbook <1220026920@student.must.edu.mo>
2024-05-09 12:33:26 +0800

    import axios and change the senBuyInfo function

--- frontend/hzmbus/src/hooks/useBuyInfo.ts
+++ frontend/hzmbus/src/hooks/useBuyInfo.ts
@@ -1,11 +1,19 @@
 //TODO: get the direction of the bus, then return the bus ticket information.
 import { Snackbar } from '@varlet/ui'
-export default function()
+import axios from 'axios'
+export default await function()
 {
     function sendBuyInfo(from:string,target:string,date:string,time:string,amount:number):boolean
     {
         if(from!=''&&target!=''&&date!=''&&time!=''&&amount>0)
         {
+            axios.post('http://localhost:3000/api/buy',{
+                from:from,
+                target:target,
+                date:date,
+                time:time,
+                amount:amount
+            })
             console.log(`You are buying tickets from ${from} to ${target} on ${date} at ${time} for ${amount} people.`);
             return false;
         }
