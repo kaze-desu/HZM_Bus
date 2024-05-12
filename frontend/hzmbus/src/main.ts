04f79de2b761dd0c192180edf3a1e8dc9d824f6f
xiaohuo <xiaohuo-desu@outlook.com>
2024-05-12 22:31:54 +0800

    small modified

--- frontend/hzmbus/src/main.ts
+++ frontend/hzmbus/src/main.ts
@@ -3,6 +3,6 @@ import Varlet from '@varlet/ui'
 import { createApp } from 'vue'
 import '@varlet/ui/es/style'
 import {createPinia} from 'pinia'
-import router from './router/Index'
+import router from '@/router/Index'
 const pinia = createPinia()
 createApp(App).use(router).use(pinia).use(Varlet).mount('#app')
